5fa9a9cbf70a5b3baaf1519a8614564e806dd1dc
Ryan Bibby <r.bibby@gmail.com>
2015-07-28 21:26:48 +0100

    Added percentage output

--- reactor.lua
+++ reactor.lua
@@ -11,12 +11,13 @@ while true do
    sleep(10)
    storedEnergy = reactor.getEnergyStored()
    currentCapacity = (storedEnergy/capacity) * 100
+   print('Power at ' .. currentCapacity .. '%')
    if currentCapacity > offPercent and reactor.getActive() then
      reactor.setActive(false)
      print('Turning reactor off')
    elseif currentCapacity < onPercent and not reactor.getActive() then
      reactor.setActive(true)
      print('Turning reactor on')
-   end 
+   end
  end  
 end
