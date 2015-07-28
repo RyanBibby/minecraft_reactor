cfa3a98065767765d46e2f7fab2e71ab09dd0c34
Ryan Bibby <r.bibby@gmail.com>
2015-07-28 20:29:53 +0100

    Initial commit

---
+++ reactor.lua
@@ -0,0 +1,22 @@
+local reactor
+local storedEnergy
+local capacity = 10000000
+local onPercent = 5
+local offPercent = 90
+local currentCapacity
+
+reactor = peripheral.wrap('back')
+while true do
+ if reactor.getConnected() then
+   sleep(10)
+   storedEnergy = reactor.getEnergyStored()
+   currentCapacity = (storedEnergy/capacity) * 100
+   if currentCapacity > offPercent and reactor.getActive() then
+     reactor.setActive(false)
+     print('Turning reactor off')
+   elseif currentCapacity < onPercent and not reactor.getActive() then
+     reactor.setActive(true)
+     print('Turning reactor on')
+   end 
+ end  
+end
